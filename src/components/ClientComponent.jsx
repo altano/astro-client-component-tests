6c458bf21c539d2b4416e3271955dde55788e750
Alan Norbauer <altano@gmail.com>
2021-12-18 14:21:31 -0800

    Add `client:load` and `client:visible` examples

---
+++ src/components/ClientComponent.jsx
@@ -0,0 +1,19 @@
+import styles from './ClientComponent.module.css';
+
+import {useState} from 'react';
+
+export default function ClientComponent() {
+  const [counter, setCounter] = useState(0);
+  const incrementCounter = () => setCounter((counter) => counter + 1);
+  return (
+    <div className={styles.container}>
+      <div className={styles.text}>
+        I'm a component, stop all the downloadin'
+      </div>
+      <div className={styles.buttonStuff}>
+        <button onClick={incrementCounter}>Click to Increment Counter</button>
+        <span>{counter}</span>
+      </div>
+    </div>
+  );
+}
